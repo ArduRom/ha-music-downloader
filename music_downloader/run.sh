5db9fd5167f262f95fcc729f9c98ddda9e676e4e
unknown <simon.hemmers97@googlemail.com>
2025-12-22 11:43:33 +0100

    Move run.sh to root and bump version

--- music_downloader/run.sh
+++ music_downloader/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Starting Music Downloader Add-on..."
+echo "Starting Music Downloader Add-on (v1.1.2)..."
 
-# Check config
 DOWNLOAD_PATH=$(bashio::config 'download_dir')
-
 echo "Download directory configured as: $DOWNLOAD_PATH"
 
-# Start the Flask App
+# Ensure /app is working dir
+cd /app
+
 echo "Starting Flask Server..."
 python3 server.py
